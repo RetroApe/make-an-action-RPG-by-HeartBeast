2f6e211e44b5aa68bf52a59ccf2e38ec0977234c
RetroApe <113673335+RetroApe@users.noreply.github.com>
2022-10-03 11:56:51 +0200

    Continuing on with the tutorial

---
+++ World/Grass.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+var GrassEffect = preload("res://Effects/GrassEffect.tscn")
+
+
+func _on_Hurtbox_area_entered(area):
+	var grassEffect = GrassEffect.instance()
+	var world = get_tree().current_scene
+	world.add_child(grassEffect)
+	grassEffect.global_position = global_position
+	
+#	get_parent().add_child(grassEffect)
+#	grassEffect.position = self.position
+	
+	queue_free()
